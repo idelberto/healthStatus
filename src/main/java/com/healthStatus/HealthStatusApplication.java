76e84538098eaa2c9019b59df000f4010f11a26d
Beto Ferreira <beto@pop-os.localdomain>
2020-05-11 20:10:55 -0300

    Iniciando Pojeto

---
+++ src/main/java/com/healthStatus/HealthStatusApplication.java
@@ -0,0 +1,13 @@
+package com.healthStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthStatus/HealthStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthStatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
