eabf3858d5e149af75b5a839cb0df2294eb124d3
Ian <ian@ian-Inspiron-5566>
2020-05-20 22:17:11 -0300

    #2 Iniciando configurações do liquibase

---
+++ src/main/java/com/healthStatus/entities/Question.java
@@ -0,0 +1,23 @@
+package com.healthStatus.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Question {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
